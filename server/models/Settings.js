77681ba0475d85755c9962e261e72761fda1b6d4
Etay <etayz@israelhayom.co.il>
2026-05-13 23:31:00 +0300

    add synagogueId to Settings model

--- server/models/Settings.js
+++ server/models/Settings.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const settingsSchema = new mongoose.Schema({
+  synagogueId: { type: mongoose.Schema.Types.ObjectId, ref: 'Synagogue' },
   shulName: { type: String, default: 'בית הכנסת הגדול' },
   city: { type: String, default: 'תל אביב' },
   parasha: { type: String, default: '' },
